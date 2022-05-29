75c7d7423623832350dcadc6ed31cbec48703187
Piotr Gardocki <pgardocki@interia.pl>
2022-05-29 23:16:35 +0200

    Update

--- zad1.py
+++ zad1.py
@@ -3,18 +3,32 @@ from stegano import lsbset
 from stegano.lsbset import generators
 import time
 import os
+import os.path
 from pathlib import Path
 
 def encode_messure(func, img, img_out, message):
+    original_size = os.stat(img).st_size
     start = time.time()
-    func(img, message).save(img_out)
+    try:
+        func(img, message).save(img_out)
+    except IndexError:
+        print(f'message len {len(message)} too large for image size {original_size}')
+        return
     end = time.time()
     print(f'encode time: {(end-start):.10f}s')
-    print('size before =', os.stat(img).st_size, ', size after =', os.stat(img_out).st_size)
+    new_size = os.stat(img_out).st_size
+    diff = new_size - original_size
+    print(f'size before = {original_size}, size after = {new_size} ' + ('(%+d)' % diff))
 
 def decode_messure(func, img):
+    if not os.path.exists(img):
+        print('missing image')
+        return
+
+    print('images size =', os.stat(img).st_size)
     start = time.time()
-    print('Message =', func(img))
+    message = func(img)
+    #print('Message =', message)
     end = time.time()
     print(f'decode time: {(end-start):.10f}s')
 
@@ -47,37 +61,47 @@ def img_encoded_path(img, method_name, message_index):
     return 'encoded_img/' + img
 
 messages = [
+    'pies',
     'ala ma kota',
     'jak ja kocham steganografie',
     'Somebody once told me, the world is gonna roll me I ain\'t the sharpest ' \
     'tool in the shed She was looking kind of dumb with her finger and her ' \
-    'thumb In the shape, of an "L" on her forehead'
+    'thumb In the shape, of an "L" on her forehead',
+    'a' * 5000,
 ]
 images = [
     'house.png',
-    'cup.png'
+    'cup.png',
+    'space.png',
+    'tree.png',
 ]
 methods = [
     ('LSB', encode_lsb, decode_lsb),
     ('LSB-eratosthenes', encode_lsb_eratosthenes, decode_lsb_eratosthenes),
-    #('LSB-fibonacci', encode_lsb_fibonacci, decode_lsb_fibonacci),
+    ('LSB-fibonacci', encode_lsb_fibonacci, decode_lsb_fibonacci),
 ]
 
 Path("encoded_img").mkdir(exist_ok=True)
 
-print('Encoding')
+print('=== Encoding ===\n')
 for method in methods:
+    k = 1
     print('Testing method:', method[0])
-    for i, message in enumerate(messages):
-        for image in images:
+    for image in images:
+        for i, message in enumerate(messages):
+            print(f'---\ntest #{str(k)}, message len = {len(message)}')
             encode_messure(method[1], img_original_path(image),
                            img_encoded_path(image, method[0], i), message)
+            k += 1
     print()
 
-print('Decoding')
+print('=== Decoding ===')
 for method in methods:
+    k = 1
     print('Testing method:', method[0])
-    for i, message in enumerate(messages):
-        for image in images:
+    for image in images:
+        for i, message in enumerate(messages):
+            print(f'---\ntest #{str(k)}, message len = {len(message)}')
             decode_messure(method[2], img_encoded_path(image, method[0], i))
+            k += 1
     print()
